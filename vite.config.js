eade9a37515a2fa79f5180173f59e2e1bf0008fd
Devanshu shekhar <devanshu.shekhar2@gmail.com>
2026-07-22 22:23:44 +0530

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio2.0/'
+  base: `/Portfolio2.0/${process.env.VITE_BASE_PATH || ''}`
 })
